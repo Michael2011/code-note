3386186f3fb24fa6077c46aa9976e8ecec30479b
luoyingbo2011 <luoyingbo@fotoable.com>
2016-06-15 11:50:56 +0800

    add java regular

--- java/demo.java
+++ java/demo.java
@@ -12,14 +12,24 @@ class demo {
 		// System.out.println(m.matches());
 		
 		System.out.println(str1.findAll("\\d+"));
+		
+		
+		Pattern p = Pattern.compile("(\"qualities\":)(.*)(,\"reporting\")");
+        String s = "\"qualities\":{\"auto\":[{\"typ:},\"reporting\"";
 		*/
-		
-		Pattern p = Pattern.compile("time=(\\d{2}:\\d{2}:\\d{2}\\.\\d{2})");
-        String s = "dasdtime=13:23:31.12";
+	
+		// Pattern p = Pattern.compile("[\'\"](http.*\\.mp3.*?)[\'\"]");
+		// Pattern p = Pattern.compile("<title>(.*?)</title>");
+        // String s = "<title>Little Girl Says Her Final Goodbyes To Her Favorite Toilet - Video Dailymotion</title>";
+
+
+		// vimeo.com
+		Pattern p = Pattern.compile("\"config_url\":\"(.*?)\"");
+		String s = "player\":{\"config_url\":\"https://player.vimeo.com/video/145937066/config?autoplay=0&byline=0&collections=1&context=Vimeo%5CController%5CClipController.main_beta&default_to_hd=1&outro=beginning&portrait=0&share=1&title=0&watch_trailer=0&s=8e5d54c0e7ab0368e56465c8f9709aa9402db7d6_1466060636\",";
+
         Matcher m = p.matcher(s);
-		
         while(m.find()){
-            System.out.println(m.group());
+            System.out.println(m.group(1));
 			
         }
 		
