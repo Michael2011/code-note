9b7fb1a8603723aa9aaf19c68a048d3e77352bc5
luoyingbo <luoyingbo@fotoable.com>
2016-06-27 09:09:11 +0000

    update data analysis

--- python/data-analysis/upload_search_urls_parse.py
+++ python/data-analysis/upload_search_urls_parse.py
@@ -3,11 +3,25 @@
 import redis
 
 r = redis.Redis(
-		host='huvi.redis.cache.windows.net',
-		password='+XfQtzIjJXMgK+4IPcAG0xUKvEsuE6stcGmsMO+yJ6s='
+        host='huvi.redis.cache.windows.net',
+        password='+XfQtzIjJXMgK+4IPcAG0xUKvEsuE6stcGmsMO+yJ6s='
 )
 
 keys = r.hkeys('info_upload_search_urls')
 for key in keys:
-	print keys 
-	break
+    if '4shared' not in key:
+        continue
+
+    values = r.hget('info_upload_search_urls', key)
+    values = values.split('#')
+
+	for value in values:
+       if '4shared' not in value:
+           continue
+
+
+       if 'music' in value:
+       
+
+
+
